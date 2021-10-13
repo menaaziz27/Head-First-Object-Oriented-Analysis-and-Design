a9f4ba15349dc3eda63ec9e2c541ba5ecb4d7116
Mena Aziz <menaaziz27@gmail.com>
2021-10-13 09:36:09 +0200

    README

---
+++ chapter-1/ricksGuitars-start/README.md
@@ -0,0 +1,13 @@
+# rickGuitar-start
+
+### the goal
+
+first things first we want the software to do what the customer wants.
+
+### the problem
+
+ Unfortunately it always return *null* because of the string case sensitive.
+
+### the solution
+
+Enums
--- chapter-1/ricksGuitars-start/src/Inventory.java
+++ chapter-1/ricksGuitars-start/src/Inventory.java
@@ -23,6 +23,8 @@ public class Inventory {
                                model, type, backWood, topWood);
     guitars.add(guitar);
   }
+  
+  
   public Guitar getGuitar(String serialNumber) {
     for (Iterator i = guitars.iterator(); i.hasNext(); ) {
       Guitar guitar = (Guitar)i.next();
@@ -32,32 +34,36 @@ public class Inventory {
     }
     return null;
   }
+  
+  
   public Guitar search(Guitar searchGuitar) {
     for (Iterator i = guitars.iterator(); i.hasNext(); ) {
-      Guitar guitar = (Guitar)i.next();
-      // Ignore serial number since that's unique
-      // Ignore price since that's unique
-      String builder = searchGuitar.getBuilder();
-      if ((builder != null) && (!builder.equals("")) &&
-          (!builder.equals(guitar.getBuilder())))
-        continue;
-      String model = searchGuitar.getModel();
-      if ((model != null) && (!model.equals("")) &&
-          (!model.equals(guitar.getModel())))
-        continue;
-      String type = searchGuitar.getType();
-      if ((type != null) && (!searchGuitar.equals("")) &&
-          (!type.equals(guitar.getType())))
-        continue;
-      String backWood = searchGuitar.getBackWood();
-      if ((backWood != null) && (!backWood.equals("")) &&
-          (!backWood.equals(guitar.getBackWood())))
-        continue;
-      String topWood = searchGuitar.getTopWood();
-      if ((topWood != null) && (!topWood.equals("")) &&
-          (!topWood.equals(guitar.getTopWood())))
-        continue;
-      return guitar;
+    	
+	      Guitar guitar = (Guitar)i.next();
+	      // Ignore serial number since that's unique
+	      // Ignore price since that's unique
+	      
+	      String builder = searchGuitar.getBuilder();
+	      if ((builder != null) && (!builder.equals("")) && (!builder.equals(guitar.getBuilder())))
+	        continue;
+	      
+	      String model = searchGuitar.getModel();
+	      if ((model != null) && (!model.equals("")) && (!model.equals(guitar.getModel())))
+	        continue;
+	      
+	      String type = searchGuitar.getType();
+	      if ((type != null) && (!searchGuitar.equals("")) && (!type.equals(guitar.getType())))
+	        continue;
+	      
+	      String backWood = searchGuitar.getBackWood();
+	      if ((backWood != null) && (!backWood.equals("")) && (!backWood.equals(guitar.getBackWood())))
+	        continue;
+	      
+	      String topWood = searchGuitar.getTopWood();
+	      if ((topWood != null) && (!topWood.equals("")) && (!topWood.equals(guitar.getTopWood())))
+	        continue;
+	      
+	      return guitar;
     }
     return null;
   }
